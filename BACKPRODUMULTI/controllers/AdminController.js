3b24359d29f79120e8368301ae51926808c84afd
Samuel2601 <saamare99@gmail.com>
2025-01-21 08:52:20 -0500

    dasda

--- BACKPRODUMULTI/controllers/AdminController.js
+++ BACKPRODUMULTI/controllers/AdminController.js
@@ -1525,16 +1525,22 @@ const obtener_detallespagos_admin = async function (req, res) {
       let pagosd = [];
 
       const config = await Config.findById(id);
-      console.log("Configuración buscada: ",config);
+      console.log("Configuración buscada: ", config);
       if (config) {
         detalle = await Dpago.find()
           .populate("idpension")
           .populate("pago")
           .lean();
         pagosd = detalle.filter(
-          (element) => element.idpension && element.idpension.idanio_lectivo == config._id || new Date(element.idpension.anio_lectivo).getTime() === new Date(config.anio_lectivo).getTime()
+          (element) =>
+            element.idpension &&
+            element.idpension.anio_lectivo &&
+            element.idpension.idanio_lectivo &&
+            (element.idpension.idanio_lectivo == config._id ||
+              new Date(element.idpension.anio_lectivo).getTime() ===
+                new Date(config.anio_lectivo).getTime())
         );
-        console.error(detalle.filter((elment)=>!elment.idpension).length);
+        console.error(detalle.filter((elment) => !elment.idpension).length);
         res.status(200).send({ data: pagosd });
       } else {
         detalle = await Dpago.find().populate("idpension");
